0666fd41000bc14db094131ab34f41dc01ec8bb9
Allen John <88543723+Allen9994@users.noreply.github.com>
2022-06-18 03:21:07 +0530

    Create hangman.cpp

---
+++ hangman.cpp
@@ -0,0 +1,140 @@
+#include <iostream>  
+#include <string.h>  
+#include <fstream>
+#include<time.h>
+#include<cstring>
+#include<ctime>
+#include<stdlib.h>
+using namespace std; 
+
+static void getSix (int *dst,int k) {
+    int sz, pos, i, src[50];
+    for (i = 0; i < sizeof(src)/sizeof(*src); i++)
+        src[i] = i + 1;
+    sz = k;
+    for (i = 0; i < 6; i++) {
+        pos = rand() % sz;
+        dst[i] = src[pos];
+        src[pos] = src[sz-1];
+        sz--;
+    }
+}
+int main()  
+{  
+    int f;
+    
+    srand((unsigned) time(0));
+    f = rand()%89;
+    
+    int i,a,b=0,c=0,g=0,j,flag,e,k,z,u,bla=0;char choice;
+    char d[20]={0};
+    
+    char n;
+    fstream file("untitled.txt");
+    string str[1000];
+    while (getline(file, str[u])) 
+    {
+       u++;
+    }
+    
+    
+    std::cout << str[f] << "\n";
+    std::string s = str[f];
+
+	char ary[s.size()];
+	s.copy(ary, s.size() + 1);
+	ary[s.size()] = '\0';
+
+	std::cout << ary ;
+    ary[strlen(ary)-1] = '\0';    
+    
+    
+    
+    a=strlen(ary);
+        for(i=0;i<a;i++)
+        {
+            if(ary[i]==(int)32)
+            {
+                d[i]=9;ary[i]=9;
+                b++;
+            }
+        }z=(a-b)/4;k=a-b;cout<<k;
+    
+    srand (time (NULL));
+    int numbers[6];
+    getSix (numbers,k);
+        
+    for(i=0;i<a;i++)
+    {
+        if(ary[i]==9)
+        {
+            cout<<" ";
+        }else
+        cout<<"- ";
+        
+    }
+    do{
+        flag=0;
+    if(bla>=3)
+    {
+        cout << "Length of String = " << strlen(ary)<<endl;
+        cout<<"Enter letter:  ";
+        cin>>n;
+    
+    for(i=0;i<a;i++)
+    {
+        if(ary[i]==n)
+        {
+            d[i]=ary[i];flag=1;
+        }
+    } }
+    else
+    {
+        i=numbers[bla];
+        d[i]=ary[i];flag=1;
+    }
+    for(i=0;i<a;i++)
+    {
+        if(flag==0)
+        {
+            g++;
+        }
+    }e=g/a;
+    if(strcmp(ary,d)==0)
+    {goto ty;
+        
+    }
+    for(i=0;i<a;i++)
+    {
+        if(d[i]==9)
+        {
+            cout<<" ";
+        }
+        if(d[i]==0)
+        {
+            cout<<"- ";
+        }else
+            cout<<d[i]<<" " ;
+    } system("clear");
+    cout<<endl<<e<<endl;
+    switch(e)
+    {
+    case 6:cout<<" _________\n";
+    case 5:cout<<"|        |\n";
+    case 4:cout<<"|	(.)\n";
+    case 3:cout<<"|	/|\\\n";
+    case 2:cout<<"|	 |\n";
+    case 1:cout<<"|	/ \\\n";
+    case 0:cout<<" ";break;
+    default:goto re;
+    }
+    
+    c++;bla++;
+    }while(c!=20);
+    ty: cout<<"YOU WIN!";
+    puts(ary);
+    return 0;
+    re: cout<<"YOU LOOSE!";
+        cout<<"\nCORRECT ANSWER IS: ";puts(ary);
+    return 0;  
+}  
